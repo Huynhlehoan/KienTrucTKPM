0052f312bb85f20de4ffb9b3f4332237e8a9df8f
Huynhlehoan <22682941.hoan@student.iuh.edu.vn>
2026-01-12 19:28:47 +0700

    Minh chung tao redis cloud

---
+++ Tuan01/HuynhLeHoan_22682941_Tuan01/src/main/java/fit/se/huynhlehoan_22682941_tuan01/HuynhLeHoan22682941Tuan01Application.java
@@ -0,0 +1,13 @@
+package fit.se.huynhlehoan_22682941_tuan01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuynhLeHoan22682941Tuan01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HuynhLeHoan22682941Tuan01Application.class, args);
+	}
+
+}
---
+++ Tuan01/HuynhLeHoan_22682941_Tuan01/src/test/java/fit/se/huynhlehoan_22682941_tuan01/HuynhLeHoan22682941Tuan01ApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.se.huynhlehoan_22682941_tuan01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuynhLeHoan22682941Tuan01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
